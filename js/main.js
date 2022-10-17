67fed774611befc145e16f18df6f367e6bf5744c
qrstuvy <nguyen.vy11@gmail.com>
2022-10-17 17:48:34 -0400

    Winning condition update

--- js/main.js
+++ js/main.js
@@ -1,16 +1,17 @@
-// const
+// constants
 let player = []
 let currentGame = []
-let count = 0;
+let round = 0
 const countValue = document.getElementById("count")
 const startGameBtn = document.querySelector('.start')
 const resetGameBtn = document.querySelector('.reset')
 const result = document.querySelector("#result")
+const yellow = document.getElementById('yellow')
 const green = document.getElementById('green')
 const blue = document.getElementById('blue')
 const red = document.getElementById('red')
-const yellow = document.getElementById('yellow')
 let possibilities = [green, blue, red, yellow]
+const rounds = document.getElementById('counter')
 
 // // possibilities: ['#A', '#B', '#C', '#D', '#E', '#F', '#G', '#H', '#I', '#J', '#K', '#L', '#M', '#N', '#O', '#P', '#Q', '#R', '#S', '#T', '#U', '#V', '#W', '#X', '#Y', '#Z'],
 
@@ -18,41 +19,77 @@ let possibilities = [green, blue, red, yellow]
 startGameBtn.addEventListener('click', newGame)
 resetGameBtn.addEventListener('click', clearGame)
 
+function enableClicks() {
+    yellow.addEventListener('click', function () {
+        player.push(yellow)
+        playerTurn();
+        console.log(player.length)
+    })
+    green.addEventListener('click', function () {
+        player.push(green)
+        playerTurn();
+        console.log(player.length)
+    })
+    blue.addEventListener('click', function () {
+        player.push(blue)
+        playerTurn();
+        console.log(player.length)
+    })
+    red.addEventListener('click', function () {
+        player.push(red)
+        playerTurn();
+        console.log(player.length)
+    })
+}
+
+function disableClicks() {
+    yellow.removeEventListener('click', function () {
+        player.push(yellow)
+        console.log(player.length)
+    })
+    green.removeEventListener('click', function () {
+        player.push(green)
+        console.log(player.length)
+    })
+    blue.removeEventListener('click', function () {
+        player.push(blue)
+        console.log(player.length)
+    })
+    red.removeEventListener('click', function () {
+        player.push(red)
+        console.log(player.length)
+    })
+}
+
 
 // Game functionality
 // Functions when CPU is creating a new item to the sequence
-// newGame() -> clearGame() -> addCount() -> generateMove() -> showSequence() -> gamePlay -> clearPlayer()
-
 
 function newGame() {
     clearGame();
     generateMove()
     startGameBtn.removeEventListener('click', newGame)
+    addRound()
 }
 
 
 function clearGame() {
     currentGame = [];
-    count = 0;
+    round = 0;
     player = []
     startGameBtn.addEventListener('click', newGame)
 }
 
-// function addCount() {
-//     game.count++;
-//     $('#clickNumber').addClass('animated fadeOutDown');
-
-//     setTimeout(function(){
-//       $('#clickNumber').removeClass('fadeOutDown').html(game.count).addClass('fadeInDown');
-//     }, 200);
-
-//     generateMove();
-//   }
+function addRound() {
+    round++;
+    rounds.innerText = round
+}
 
 // Generates a random move/color
 function generateMove() {
     currentGame.push(possibilities[(Math.floor(Math.random() * possibilities.length))]);
     showSequence();
+    disableClicks()
     console.log(currentGame)
 }
 
@@ -64,16 +101,16 @@ function showSequence() {
         i++;
         if (i >= currentGame.length) {
             clearInterval(sequence);
-        }
+        } enableClicks()
     }, 600)
     clearPlayer();
 }
 
 
-function gamePlay(possibilities) {
-    possibilities.classList.add('active');
+function gamePlay(sequence) {
+    sequence.classList.add('active');
     setTimeout(function () {
-        possibilities.classList.remove('active');
+        sequence.classList.remove('active');
     }, 300);
 }
 
@@ -82,11 +119,33 @@ function clearPlayer() {
 }
 
 
-
 // Functions for checking players moves
 
-
+function playerTurn() {
+    if (player[player.length - 1] !== currentGame[player.length - 1]) {
+        console.log('game over!')
+        clearGame();
+    } else {
+        console.log('nice!');
+        let check = player.length === currentGame.length;
+        if (check) {
+            if (round == 10) {
+                console.log('Congrats!');
+            } else {
+                console.log('Next round!');
+                nextLevel();
+            }
+        }
+    }
+}
 
 function nextLevel() {
     generateMove()
+    addRound()
 }
+
+
+
+
+
+
