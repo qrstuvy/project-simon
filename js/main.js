1d36a6785ce53da30a8fde8403c7475cfd419c29
qrstuvy <nguyen.vy11@gmail.com>
2022-10-17 13:39:22 -0400

    Panel light up function

--- js/main.js
+++ js/main.js
@@ -2,42 +2,62 @@
 let player = []
 let currentGame = []
 let count = 0;
-let possibilities = ['green', 'blue', 'red', 'yellow']
 const countValue = document.getElementById("count")
-const startGameBtn = document.querySelector('.start').addEventListener('click', newGame)
+const startGameBtn = document.querySelector('.start')
+const resetGameBtn = document.querySelector('.reset')
 const result = document.querySelector("#result")
-
 const green = document.getElementById('green')
 const blue = document.getElementById('blue')
 const red = document.getElementById('red')
 const yellow = document.getElementById('yellow')
+let possibilities = [green, blue, red, yellow]
 
 // // possibilities: ['#A', '#B', '#C', '#D', '#E', '#F', '#G', '#H', '#I', '#J', '#K', '#L', '#M', '#N', '#O', '#P', '#Q', '#R', '#S', '#T', '#U', '#V', '#W', '#X', '#Y', '#Z'],
+
+// Event listeners
+startGameBtn.addEventListener('click', newGame)
+resetGameBtn.addEventListener('click', clearGame)
 
 
 // Game functionality
 // Functions when CPU is creating a new item to the sequence
+// newGame() -> clearGame() -> addCount() -> generateMove() -> showSequence() -> gamePlay -> clearPlayer()
+
 
 function newGame() {
     clearGame();
     generateMove()
+    startGameBtn.removeEventListener('click', newGame)
 }
 
 
 function clearGame() {
     currentGame = [];
     count = 0;
+    player = []
+    startGameBtn.addEventListener('click', newGame)
 }
+
+// function addCount() {
+//     game.count++;
+//     $('#clickNumber').addClass('animated fadeOutDown');
+
+//     setTimeout(function(){
+//       $('#clickNumber').removeClass('fadeOutDown').html(game.count).addClass('fadeInDown');
+//     }, 200);
+
+//     generateMove();
+//   }
 
 // Generates a random move/color
 function generateMove() {
     currentGame.push(possibilities[(Math.floor(Math.random() * possibilities.length))]);
-    startSequence();
+    showSequence();
     console.log(currentGame)
 }
 
 
-function startSequence() {
+function showSequence() {
     let i = 0;
     let sequence = setInterval(function () {
         gamePlay(currentGame[i]);
@@ -45,16 +65,16 @@ function startSequence() {
         if (i >= currentGame.length) {
             clearInterval(sequence);
         }
-    }, 1000)
+    }, 600)
     clearPlayer();
 }
 
 
-function gamePlay(event) {
-    event.target.classList.add('active');
+function gamePlay(possibilities) {
+    possibilities.classList.add('active');
     setTimeout(function () {
-        event.target.classList.remove('active');
-    }, 500);
+        possibilities.classList.remove('active');
+    }, 300);
 }
 
 function clearPlayer() {
@@ -63,18 +83,10 @@ function clearPlayer() {
 
 
 
+// Functions for checking players moves
 
-// Functions for player to repeat the pattern
 
-// function playerTurn(){
-//     - Waits for player input and check to see if the correct choice has been clicked
-// }
 
-// function nextLevel(){
-//     - Calls on function to add to counter
-// }
-
-// function addToCounter(){
-//     - Adds the number of clicks to the counter to allow player to know what round they are on
-//     - Calls on generateMoves function to repeat pattern and create a new move at the end
-// }
+function nextLevel() {
+    generateMove()
+}
