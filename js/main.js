f217bd8b5e2830c494d4872566570a571e2ddb4b
qrstuvy <nguyen.vy11@gmail.com>
2022-10-19 18:32:35 -0400

    New victory image

--- js/main.js
+++ js/main.js
@@ -1,17 +1,15 @@
-// Constants
+// Variables--------------------------------------------------------------------------------------------------
 const rounds = document.getElementById('counter')
 const gameText = document.getElementById('game-text')
-const timerText = document.getElementById('timer-text')
-const imageGrid = document.querySelector('#images-grid')
+let possibilities = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
-// variables
+
+// Initial state of game----------------------------------------------------------------------------------------
 let player = []
 let currentGame = []
 let round = 0
-let possibilities = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-let time = 5
 
-// Game Buttons
+// Game Buttons-------------------------------------------------------------------------------------------------
 const playGameBtn = document.getElementById('play-btn')
 
 const instructionsBtn = document.getElementById('instructions-btn')
@@ -27,24 +25,19 @@ resetGameBtn.addEventListener('click', clearGame)
 const backBtn = document.getElementById('back')
 
 
-// Sections, divs, and others
+// Sections, divs, and others------------------------------------------------------------------------------------
 const instructionsPage = document.getElementById('instructions-div')
+// Section aside from the game container
 const textSection = document.getElementById('text-section')
+// Div that contains the entire grid, game and buttons
 const imagesContainer = document.getElementById('images-container')
+// Div that wraps the grid
 const gameDiv = document.getElementById('game-div')
+// Div where letters are placed on the grid
+const imagesGrid = document.querySelector('#images-grid')
 
 
-// Loads game page
-function loadGame() {
-    textSection.classList.remove('hide')
-    imagesContainer.classList.remove('hide')
-    gameDiv.classList.remove('hide')
-    playGameBtn.classList.add('hide')
-}
-
-
-
-// Event Listeners
+// Event Listeners for buttons-----------------------------------------------------------------------------------
 backBtn.addEventListener('click', () => {
     textSection.classList.add('hide')
     imagesContainer.classList.add('hide')
@@ -64,8 +57,16 @@ okayBtn.addEventListener('click', () => {
 })
 
 
-// Enable/disable letter clicks
+// Loads game page---------------------------------------------------------------------------------------------------
+function loadGame() {
+    textSection.classList.remove('hide')
+    imagesContainer.classList.remove('hide')
+    gameDiv.classList.remove('hide')
+    playGameBtn.classList.add('hide')
+}
 
+
+// Enable/disable player inputs on click during specific portions of the game----------------------------------------
 function playerClick(event) {
     event.target.classList.add('active')
     setTimeout(function () {
@@ -77,28 +78,24 @@ function playerClick(event) {
 }
 
 function enableClicks() {
-    imageGrid.classList.remove('unclickable')
-    imageGrid.addEventListener('click', playerClick)
+    imagesGrid.classList.remove('unclickable')
+    imagesGrid.addEventListener('click', playerClick)
 }
-
 
 function disableClicks() {
-    imageGrid.classList.add('unclickable')
+    imagesGrid.classList.add('unclickable')
 }
 
 
-
-// Game functionality
-// Functions when CPU is creating a new item to the sequence
-
+// Game functionality/functions when CPU is creating a new item to the sequence---------------------------------
+// New game initialization when pressing play game button
 function newGame() {
-    clearGame()
     generateMove()
     startGameBtn.removeEventListener('click', newGame)
     addRound()
 }
 
-
+// Resets game to initial state on reset button click
 function clearGame() {
     currentGame = []
     player = []
@@ -106,23 +103,22 @@ function clearGame() {
     rounds.innerText = round
     gameText.innerText = "Let's go!"
     startGameBtn.addEventListener('click', newGame)
+    initialImage()
 }
-
+// Adds to round counter/text everytime player proceeds to next level
 function addRound() {
     round++;
     rounds.innerText = round
 }
 
-// Generates a random move/color
+// Function to generate a random move
 function generateMove() {
     currentGame.push(possibilities[(Math.floor(Math.random() * possibilities.length))])
     setTimeout(() => {
         showSequence()
     }, "600")
-    console.log(currentGame)
 }
-
-
+// Function that replays/shows entire sequence to player in addition to the newly generated move
 function showSequence() {
     let i = 0;
     let sequence = setInterval(function () {
@@ -134,43 +130,37 @@ function showSequence() {
         }
     }, 600)
     clearPlayer()
-    console.log(player)
 }
 
-
+// Function nested into the showSequence function to flash letters by adding .active containing a white bg-color
 function gamePlay(sequence) {
-    const image = imageGrid.querySelector(`#${sequence}`)
+    const image = imagesGrid.querySelector(`#${sequence}`)
     image.classList.add('active')
     setTimeout(function () {
         image.classList.remove('active')
     }, 300)
 }
 
+// Clears the player's array to initial state prior to player's turn
 function clearPlayer() {
     player = []
 }
 
 
-// Functions for checking players moves
-
-
+// Function for checking player's input and comparing to currentGame's input--------------------------------------------
 function playerTurn() {
     if (player[player.length - 1] !== currentGame[player.length - 1]) {
-        console.log('game over!')
         gameText.innerText = "Oh no, nice try! Game over."
         disableClicks()
     } else {
-        console.log('nice!')
         if (player.length === currentGame.length) {
-            if (round == 10) {
-                console.log('Congrats! You win!!')
-                gameText.innerText = "Congrats! You win!!"
-            } else if (round == 5) {
-                console.log('WOW LOOK AT YOU!')
+            if (round == 5) {
+                gameText.innerText = "Congratulations! You did it!"
+                changeImage()
+            } else if (round == 2) {
                 gameText.innerText = "WOW LOOK AT YOU! Keep going!"
                 nextLevel();
             } else {
-                console.log('Next round!')
                 gameText.innerText = "Nice! Next round!"
                 nextLevel();
             }
@@ -179,6 +169,18 @@ function playerTurn() {
 }
 
 
+// Image change function depending on round
+function changeImage() {
+    document.querySelector('.starting-image').classList.add('hide')
+    document.querySelector('.victory').classList.remove('hide')
+}
+
+function initialImage() {
+    document.querySelector('.starting-image').classList.remove('hide')
+    document.querySelector('.victory').classList.add('hide')
+}
+
+// Function to progress to next level that loops back to generate move, adds a round and disables player input
 function nextLevel() {
     generateMove()
     addRound()
